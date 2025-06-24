6871ec0bae300e169426d4918ed878bc80c93425
tushara04 <tusharagarwal1016@gmail.com>
2025-06-24 12:03:36 +0530

    added

---
+++ simulation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+cards = np.array([])
+
+card_suits = ["spade", "club","diamond", "heart"]
+face_cards = ["jack", "queen", "king"]
+color = ["red","black"]
+
+def card_game():
+    print("hello")
